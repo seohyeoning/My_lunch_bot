97ed7d945163fa36c73220ef63e1ec8df68733f2
Seo-hyeon Park <tjgus9190@korea.ac.kr>
2024-12-26 14:10:06 +0900

    Update lunch_bot.py

--- My_Chatbot_gm/lunch_bot.py
+++ My_Chatbot_gm/lunch_bot.py
@@ -4,8 +4,9 @@ import os
 import streamlit as st
 import openai
 
-openai.api_key = st.secrets["OPENAI_API_KEY"]
-google_api_key = st.secrets["google_api_key"]
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+google_api_key = os.getnev("google_api_key")
 
 # GPT 모델을 사용한 점심 추천
 def get_lunch_recommendations(weather, preference, mood):
